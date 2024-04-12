505c43946c32da428ef22b8f14201c61eb0ada06
Marco Tulio <marcotulio_iub@hotmail.com>
2024-04-12 10:18:51 -0300

    Servidor configurado

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
+import { ApolloServer } from "apollo-server";
+import "reflect-metadata";
 
+async function main() {
+  const server = new ApolloServer({});
+
+  const { url } = await server.listen();
+
+  console.log(`Server running on ${url}`);
+}
+
+main();
